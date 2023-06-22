0d8cc70a01f10e56c6f9ab3bdbb7e63c8507f8c6
nevel <diegonevel@gmail.com>
2023-06-22 18:07:41 +0200

    Agrego rutas GET al back

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send("Estoy en Home");
+});
+
+router.get('/detail-product', function(req, res, next) {
+  res.send("Estoy en detalle de producto");
+});
+
+router.get('/registrarme', function(req, res, next) {
+  res.send("Estoy en registrarme");
+});
+
+router.get('/cart', function(req, res, next) {
+  res.send("Estoy en cart");
+});
+
+router.get('/inicio-sesion', function(req, res, next) {
+  res.send("Estoy en inisio de sesion");
 });
 
 module.exports = router;
